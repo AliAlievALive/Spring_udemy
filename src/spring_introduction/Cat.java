73dd7c01945d5ab1aef9f9362f3f82f7bc7514f1
Almir <almir.almiev@tatar.ru>
2021-07-22 20:06:16 +0300

    @PreDestroy и @PostConstruct lesson

--- src/spring_introduction/Cat.java
+++ src/spring_introduction/Cat.java
@@ -2,8 +2,25 @@ package spring_introduction;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component("catBean")
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("init method");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("destroy method");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow-meow");
