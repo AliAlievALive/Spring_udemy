1f1d7736c71f0f1b8c5d05f8f1a5f159cd3a27d9
almiev <madarat@mail.ru>
2021-09-20 21:01:38 +0300

    One-to-one Lesson 47

--- src/hibernate_test2/Test1.java
+++ src/hibernate_test2/Test1.java
@@ -1,4 +1,4 @@
-package hibernate_test;
+package hibernate_test2;
 
 import hibernate_test.entity.Employee;
 import org.hibernate.Session;
@@ -6,26 +6,22 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
-public class Test5 {
+public class Test1 {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration()
+
+        try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
-                .buildSessionFactory();
-        try {
+                .buildSessionFactory()) {
             Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Aleksandr", "Ivanov", "IT", 600);
             session.beginTransaction();
-//            Employee employee = session.get(Employee.class, 1);
-//            session.delete(employee);
-            session.createQuery("delete Employee " +
-                    "where name = 'Aleksandr'").executeUpdate();
-
+            session.save(emp);
             session.getTransaction().commit();
+
             session.close();
 
             System.out.println("done");
-        } catch (Exception e) {
-            factory.close();
         }
     }
 }
