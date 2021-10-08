d6742ae0852f2800149b34fd9a5b5ed458e34f39
almiev <madarat@mail.ru>
2021-10-08 21:47:01 +0300

    One-to-Many (Bi-directional) 2 Lesson 50

--- src/hibernate_One_to_one/Test2.java
+++ src/hibernate_One_to_one/Test2.java
@@ -1,13 +1,13 @@
-package hibernate_test2;
+package hibernate_One_to_one;
 
-import hibernate_test2.entity.Detail;
-import hibernate_test2.entity.Employee;
+import hibernate_One_to_one.entity.Detail;
+import hibernate_One_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
 
         try (SessionFactory factory = new Configuration()
@@ -16,30 +16,29 @@ public class Test1 {
                 .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
              Session session = factory.getCurrentSession()) {
-//            Session session = factory.getCurrentSession();
-//            Employee employee = new Employee("Oleg", "Ibanov", "Sale", 700);
-//            Detail detail = new Detail("Msc", "321321321", "@ya.ru");
+//            Employee employee = new Employee("Kostya", "Ivanov", "HR", 850);
+//            Detail detail = new Detail("NY", "2222222222", "Kostya@mail.ru");
 //
 //            employee.setEmpDetail(detail);
+//            detail.setEmployee(employee);
 //            session.beginTransaction();
 //
-//            session.save(employee);
+//            session.save(detail);
 //
 //            session.getTransaction().commit();
 //            System.out.println("done");
-//////////////////////////////////////////////////////////////////
-//            session.beginTransaction();
-//
-//            Employee employee = session.get(Employee.class, 10);
-//            System.out.println(employee.getDepartment());
-//
-//            session.getTransaction().commit();
-//            System.out.println("done");
-//////////////////////////////////////////////////////////////////
-            session.beginTransaction();
 
-            Employee employee = session.get(Employee.class, 2);
-            session.delete(employee);
+//            session.beginTransaction();
+//            Detail detail = session.get(Detail.class, 4);
+//            System.out.println(detail.getEmployee());
+//
+//            session.getTransaction().commit();
+//            System.out.println("done");
+
+            session.beginTransaction();
+            Detail detail = session.get(Detail.class, 1);
+            detail.getEmployee().setEmpDetail(null);
+            session.delete(detail);
 
             session.getTransaction().commit();
             System.out.println("done");
