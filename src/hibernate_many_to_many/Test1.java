cbf0713be57cf8f731524dfc17c6a9a51e3f5b87
almiev <madarat@mail.ru>
2021-10-09 18:11:07 +0300

    ManyToMany 2 Lesson 54

---
+++ src/hibernate_many_to_many/Test1.java
@@ -0,0 +1,91 @@
+package hibernate_many_to_many;
+
+import hibernate_many_to_many.entity.Child;
+import hibernate_many_to_many.entity.Section;
+import hibernate_one_to_many_bi.entity.Department;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Child.class)
+                .addAnnotatedClass(Section.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession())
+        {
+//            Section section1 = new Section("Golf");
+//            Child child1 = new Child("Olya", 15);
+//            Child child2 = new Child("David", 4);
+//            Child child3 = new Child("Pavlic", 9);
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+//
+//            session.beginTransaction();
+//
+//            session.persist(section1);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!!!");
+//          ****************************************************
+
+//            Section section1 = new Section("Volleyball");
+//            Section section2 = new Section("Chess");
+//            Section section3 = new Section("Math");
+//            Child child1 = new Child("Igor", 10);
+//            child1.addSectionToChild(section1);
+//            child1.addSectionToChild(section2);
+//            child1.addSectionToChild(section3);
+//
+//            session.beginTransaction();
+//
+//            session.save(child1);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!!!");
+//          ****************************************************
+//            session.beginTransaction();
+//
+//            Section section = session.get(Section.class, 1);
+//
+//            System.out.println(section);
+//            System.out.println(section.getChildren());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!!!");
+//          ****************************************************
+//            session.beginTransaction();
+//
+//            Child child = session.get(Child.class, 5);
+//
+//            System.out.println(child);
+//            System.out.println(child.getSections());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!!!");
+//          ****************************************************
+//            session.beginTransaction();
+//
+//            Section section = session.get(Section.class, 11);
+//
+//            session.delete(section);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!!!");
+//          ****************************************************
+            session.beginTransaction();
+
+            Child child = session.get(Child.class, 6);
+
+            session.delete(child);
+
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+        }
+    }
+}
